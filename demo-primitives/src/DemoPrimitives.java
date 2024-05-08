ab0c960499c5e321b01410ee5ad2f32d1f092b14
mario <moonfunpeople@gmail.com>
2024-05-08 21:56:16 +0800

    updated

---
+++ demo-primitives/src/DemoPrimitives.java
@@ -0,0 +1,11 @@
+public class DemoPrimitives {
+  public static void main(String[] args) {
+    int x=6;
+    int z=-6;
+    int y=4;
+    int total= x+y*z;
+    System.out.println(total);
+int reminder =10/3
+
+  }
+}
---
+++ demo-primitives/src/DemoString.java
@@ -0,0 +1,12 @@
+public class DemoString {
+public static void main(String[] args) {
+  String x="hello";
+  String y="#$%^&*&^%$#";
+  String dollar ="$10";
+
+  String withSpace = "hello   H ello";
+  System.out.println(withSpace);
+  String z= x + dollar;
+  System.out.println(z);//hello$10
+}
+}
